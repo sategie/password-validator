ebe5c6a792ac932bfbc3d205886e9e1dc7088883
sategie <sategie@yahoo.com>
2024-08-21 17:36:07 +0200

    Remove Scanner class

--- src/main/java/org/example/PasswordValidator.java
+++ src/main/java/org/example/PasswordValidator.java
@@ -1,10 +1,10 @@
 package org.example;
 
-import java.util.Scanner;
-
 public class PasswordValidator {
     public static void main(String[] args) {
-        Scanner validate = new Scanner(System.in);//Create Scanner object
-        System.out.println("Enter your password: ");
+        System.out.println("Password Validator");
     }
+
+
+
 }
